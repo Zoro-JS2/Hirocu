1030878bc60a9025c3a12458890dc035797bafcf
Rigidbody-JS <66162438+Zoro-JS2@users.noreply.github.com>
2020-12-21 19:14:33 +0200

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,10 @@
+$ heroku login
+$ git config --global --add user.email "dmytro.hedziuk@nure.ua"
+$ git config --global --add user.name "Dmytro"
+$cd appengine/hello-world/
+$cd appengine/hello-world/
+$ git init
+$ heroku git:remote -a hellocloudpaas
+$ git add .
+$ git commit -am "Some comment"
+$ git push heroku master
